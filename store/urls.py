e9ae49c1570fae40aa4289a9f7f4fe4e98498033
mdmarghe <margheritanuccio98@gmail.com>
2023-09-12 15:12:15 +0200

    homepage html

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	#Leave as empty string for base url
-	path('', views.store, name="store"),
+	path('', views.homepage, name="homepage"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
 
