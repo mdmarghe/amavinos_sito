3df0c16878f98b57d65cce6f04aa3154b612f8b9
mdmarghe <margheritanuccio98@gmail.com>
2023-09-13 14:41:23 +0200

    less 22

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
-   
+    url(r'^signup/$', views.signup_view, name="signup"),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
 
 def signup_view(request):
     if request.method == 'POST':
@@ -11,3 +13,20 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form': form })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            user = form.get_user()
+            login(request, user)
+            return redirect('..catas/catas_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form': form })
+
+def logout_view(request):
+    if request.method == 'POST':
+            logout(request)
+            return redirect('catas_list')
