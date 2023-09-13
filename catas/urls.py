5249ab73299cd0d77f9a3e15713496ca358252d9
mdmarghe <margheritanuccio98@gmail.com>
2023-09-13 14:07:43 +0200

    prvo a collegare catas al acquistp

--- catas/urls.py
+++ catas/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
 	
 	path('', views.catas_list, name="catas_list"),
+    
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
 	#path('cart/', views.cart, name="cart"),
 	#path('checkout/', views.checkout, name="checkout"),
 
