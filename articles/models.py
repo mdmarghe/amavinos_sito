0574d7d4ab3645944aa26d38ad66a9430b5509df
mdmarghe <margheritanuccio98@gmail.com>
2023-09-13 20:31:06 +0200

    aiuto

--- articles/models.py
+++ articles/models.py
@@ -7,7 +7,8 @@ class Article(models.Model):
     body=models.TextField(blank=True)
     date=models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
-    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=None)
+
     #add thumbnail y author
 
     def __str__(self):
